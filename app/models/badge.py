f912342bdd8ac6072063fb8f97e1af8723db7477
Alex <ajtravis2@gmail.com>
2023-02-10 16:20:35 -0500

    added userbadge table

--- app/models/badge.py
+++ app/models/badge.py
@@ -1,4 +1,5 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
+from .userbadge import userbadges
 
 class Badge(db.Model):
     __tablename__ = 'badges'
@@ -8,13 +9,12 @@ class Badge(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     beer_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("beers.id")))
     brewery_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("breweries.id")))
-    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
     icon = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text, nullable=False)
 
     badge_beer = db.relationship("Beer", back_populates="beer_badge")
     badge_brewery = db.relationship("Brewery", back_populates="brewery_badge")
-    badge_user = db.relationship("User", back_populates="user_badge")
+    badge_user = db.relationship("User", secondary=userbadges, back_populates="user_badge")
 
 
     def to_dict(self):
