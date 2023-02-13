28fac96c9a5d9de01ffb36135deae352ebd469c1
Philip <seongju.lee90@gmail.com>
2023-02-13 14:21:29 -0800

    did read for badges need to do create

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import User, userbadges, Badge, db
-
+from sqlalchemy import text
 
 user_routes = Blueprint('users', __name__)
 
@@ -31,8 +31,34 @@ def user_badges(id):
     """
         Query for a user's badges
     """
-    user = User.query.get(id)
+    sql = text('select * from userbadges')
+    result = db.engine.execute(sql)
 
-    badges = db.session.query(Badge).join(User).filter(Badge.id == id)
+    list = []
+    for r in result:
+        if (r[0] == id):
+            badge = Badge.query.get(r[1])
+            list.append(badge.to_dict())
 
-    print('asndfasidlnfasldfkjansdflkajsdfklas', badges)
+    return {'badges': list}
+
+    # userId = id
+    # badges = Badge.query.filter(userId.in_(Badge.badge_user)).all()
+
+    # badges = Badge.query.join(Badge.badge_user).all()
+    # badges = Badge.query.join(User.user_badge).all()
+    # badges = badges.filter(badges.users == userId)
+    # badges = User.query.all()
+
+    # list = []
+    # for badge in badges:
+    #     i = badge.to_dict()
+    #     print('iiiiiiiiiiiiiiiiiiiiiiiiiiiii', i)
+    #     # print('bagdge', badge)
+    #     # print('badge.badge_user', badge.badge_user)
+    #     # print('badge.user_badge', User.user_badge)
+    #     if (badge.badge_user == userId):
+    #         list.append(badge)
+    # badges = db.session.query(Badge) \
+    #                    .join(User)  \
+    #                    .filter(Badge.badge_user == id)
