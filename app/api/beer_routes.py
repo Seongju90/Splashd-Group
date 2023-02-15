39d23008695e131835c4e4c568dfe23d264d4e52
Alex <ajtravis2@gmail.com>
2023-02-15 17:22:33 -0500

    added functionality to earn badges

--- app/api/beer_routes.py
+++ app/api/beer_routes.py
@@ -123,5 +123,24 @@ def create_review(id):
 
         db.session.add(newReview)
         db.session.commit()
-        return newReview.to_dict()
+
+
+        # check badges
+        user = User.query.get(current_user.id)
+        # when more badges exist for each beer we can have a list of beer badges
+        earned_badges = Badge.query.filter(Badge.beer_id == id).all()
+
+        newReview = newReview.to_dict()
+        newReview['badges_earned'] = []
+
+        for badge in earned_badges:
+            print(badge, "checking if you earned any badges for this beer")
+            if not badge.id in user.badge_info()["badges"]:
+                print("badge earned!!!!!!!!!!!!!!!!!!!!!!")
+                user.user_badges.append(badge)
+                newReview['badges_earned'].append(badge.id)
+
+        db.session.commit()
+
+        return newReview
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
