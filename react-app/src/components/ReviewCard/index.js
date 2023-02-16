54a60a5dd5f07958d3d3fe387712d97ad77204ac
Alex <ajtravis2@gmail.com>
2023-02-15 20:38:15 -0500

    fixing conflicts

--- react-app/src/components/ReviewCard/index.js
+++ react-app/src/components/ReviewCard/index.js
@@ -48,6 +48,7 @@ export default function ReviewCard({username, review, beer, location}) {
                                     rev={review} />}
                         />
                     </div>
+                    }
                 </div>
                 { location ? null :<div className="review-beer-container">
                     <div className="review-sub-beer-container">
