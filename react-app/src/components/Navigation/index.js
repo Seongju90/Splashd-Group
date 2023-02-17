6a2c109926c9d484ad1db74bc0112de06afffbc6
brylee8295 <107957712+brylee8295@users.noreply.github.com>
2023-02-16 16:10:46 -0800

    contact us

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import ProfileButton from './ProfileButton';
+import ContactUsModal from '../ContactUsModal';
+import OpenModalButton from '../OpenModalButton';
 
 function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
@@ -16,6 +18,14 @@ function Navigation({ isLoaded }) {
 					onClick={() => window.scroll(0, 0) || history.push('/')}
 				>Splashd</div>
 			</div >
+			<div className='contact-us'>
+				<OpenModalButton
+					buttonText="Contact Us"
+					// onItemClick={closeMenu}
+					modalComponent={<ContactUsModal />}
+				/>
+
+			</div>
 			<div className='navcont navright'>
 				<ProfileButton className='navcont navright' user={sessionUser} />
 				<div>
