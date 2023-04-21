1d9491c00b9521b1a09aedcfb3a5706868d1043c
Dex <pdassaf@gmail.com>
2023-04-21 09:14:10 -0700

    fixed the contact us

--- react-app/src/components/ContactUsModal/index.js
+++ react-app/src/components/ContactUsModal/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useModal } from "../../context/Modal";
 import gitcon from '../../assets/gitcon.png'
 import linkcon from '../../assets/linkcon.png'
+import '../../zCSS/about.css'
 
 function ContactUsModal() {
     const { closeModal } = useModal();
@@ -11,25 +12,27 @@ function ContactUsModal() {
     // If you contribute again, feel free to shift your name and link divs up to current contributors
     // Unfortunately, since we can't see, to find cohesion, I am gonna take this across what i consider the finish line
     //it would be a ton faster with help!!!!
+    // Alex: used yours as an example cause i figure youll see this 
+    //just comment in the "personcard"
     return (
         <div className='contact-us'>
             <div className="modal-whole">
-                <div className="modal-header">
+                <div className="modal-header" id='headabout'>
                     <div className="modal-title">Contact Us!</div>
                     <div className="modal-exit"
                         onClick={() => closeModal()
                         }>X
                     </div>
                 </div>
-                <div className="buddy-info-container">
+                <div className="aboutinfo">
                     <div className="buddy-into-top-container">
 
-                        <div className="buddy-info-field-alex">
+                        <div className="abouttitle one">
                             Current Contributors:
                         </div>
-                        <div className="buddy-info-field-dex">
-                            P. Dexter Assaf:
-                            <div>
+                        <div className="currentbox">
+                            <div className='personcard'>
+                                P. Dexter Assaf:
                                 <div className="gitcon" onClick={() => window.open('https://github.com/D0RK5TER', '_blank')}
                                     style={{ cursor: 'pointer' }}
                                 >
@@ -48,15 +51,33 @@ function ContactUsModal() {
 
 
                             </div>
+                            {/* <div className='personcard'>
+                                Alex Travis:
+                                <div className="gitcon" onClick={() => window.open('https://github.com/ajtravis', '_blank')}
+                                    style={{ cursor: 'pointer' }}
+                                >
+                                    <img src={gitcon}
+                                        className="gitcon"
+                                        style={{ float: 'left' }}
+                                        alt='my buttons'
+                                    />
+                                </div>
+
+                                <div className="linkcon" style={{ cursor: 'pointer' }} onClick={() => window.open('https://www.linkedin.com/in/ajtravis93/', '_blank')}>
+                                    <img src={linkcon}
+                                        alt='my buttons'
+                                    />
+                                </div>
+
+
+                            </div> */}
                         </div>
                     </div>
-                    <div className="buddy-into-bottom-container">
-                        <div className="buddy-info-field-alex">
-                            Past Contributors:
-                        </div>
+                    <div className="abouttitle two">
+                        Past Contributors:
                         <div className="buddy-info-field-bryan">
-                            Alex Travis:
-                            <div>
+                            <div className='personcard'>
+                                Alex Travis:
                                 <div className="gitcon" onClick={() => window.open('https://github.com/ajtravis', '_blank')}
                                     style={{ cursor: 'pointer' }}
                                 >
@@ -75,8 +96,8 @@ function ContactUsModal() {
 
 
                             </div>
-                            Bryan Sang Heon Lee:
-                            <div>
+                            <div className='personcard'>
+                                Bryan Sang Heon Lee:
                                 <div className="gitcon" onClick={() => window.open('https://github.com/brylee8295', '_blank')}
                                     style={{ cursor: 'pointer' }}
                                 >
@@ -95,8 +116,8 @@ function ContactUsModal() {
 
 
                             </div>
-                            Philip Lee:
-                            <div>
+                            <div className='personcard'>
+                                Philip Lee:
                                 <div className="gitcon" onClick={() => window.open('https://github.com/Seongju90', '_blank')}
                                     style={{ cursor: 'pointer' }}
                                 >
@@ -116,8 +137,8 @@ function ContactUsModal() {
 
                             </div>
                         </div>
-                        
                     </div>
+
                 </div>
             </div>
         </div>
